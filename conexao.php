83a03be4401dec175e2eae3b2112ecd00e38b7bd
thiagomag94 <thiagomag94@gmail.com>
2021-03-09 00:58:25 -0300

    atualizando

--- conexao.php
+++ conexao.php
@@ -1,10 +1,17 @@
 <?php
 
-$hostname = "localhost";
-$user = "root";
-$password = "";
-$database = "cadastros";
+
+
+
+$hostname = "form.criuxypwt58s.sa-east-1.rds.amazonaws.com";
+$user = "admin";
+$password = "D4rk0102";
+$database = "formulario";
 $conexao = mysqli_connect($hostname,$user,$password,$database);
+
+if(isset($_POST['enviar'])){
+    
+}
 
 if (!$conexao){
     print "Falha na conexão com o banco de dados";
---
+++ teste.py
@@ -0,0 +1,17 @@
+import pymysql
+from flask import request
+
+if request.method == 'POST':
+    user =  request.form['nome']
+    email =  request.form['email'] 
+
+    
+db = pymysql.connect(host='form.criuxypwt58s.sa-east-1.rds.amazonaws.com',
+                      user='admin',
+                      password='D4rk0102',
+                      database='testepython')
+cursor = db.cursor()
+cursor.execute("USE testepython")
+cursor.execute("CREATE TABLE IF NOT EXISTS teste2(nome varchar(50), email varchar(40))")
+
+
